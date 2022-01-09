ab6f519eb0c68ce5aaf09cb047645b32a81031c4
github-action-benchmark <github@users.noreply.github.com>
2022-01-09 10:28:36 +0000

    add Benchmark (benchmarkjs) benchmark result for 235135314ea5b849470401725995da25dda30003

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641460932986,
+  "lastUpdate": 1641724116967,
   "repoUrl": "https://github.com/speedy-js/napi-template",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,44 @@ window.BENCHMARK_DATA = {
             "range": "±0.13%",
             "unit": "ops/sec",
             "extra": "98 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "andywangsy@gmail.com",
+            "name": "h-a-n-a",
+            "username": "h-a-n-a"
+          },
+          "committer": {
+            "email": "andywangsy@gmail.com",
+            "name": "h-a-n-a",
+            "username": "h-a-n-a"
+          },
+          "distinct": true,
+          "id": "235135314ea5b849470401725995da25dda30003",
+          "message": "ci: fix publish",
+          "timestamp": "2022-01-09T18:27:22+08:00",
+          "tree_id": "8279968b5cdaebe5c3cf3c07a86557926b396783",
+          "url": "https://github.com/speedy-js/napi-template/commit/235135314ea5b849470401725995da25dda30003"
+        },
+        "date": 1641724115961,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "add#native",
+            "value": 22765018,
+            "range": "±0.28%",
+            "unit": "ops/sec",
+            "extra": "96 samples"
+          },
+          {
+            "name": "add#node",
+            "value": 714360566,
+            "range": "±0.14%",
+            "unit": "ops/sec",
+            "extra": "96 samples"
           }
         ]
       }
