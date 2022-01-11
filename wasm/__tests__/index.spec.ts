dfb4ad0a549571a9b212c1759260f52bbbb11625
h-a-n-a <andywangsy@gmail.com>
2022-01-11 18:03:36 +0800

    chore: add web-assembly publish

--- wasm/__tests__/index.spec.ts
+++ wasm/__tests__/index.spec.ts
@@ -1,8 +1,8 @@
-import assert from "assert"
-import { add } from "../pkg-node/speedy_sourcemap_wasm"
+import assert from 'assert'
+import { add } from '../pkg-node'
 
-describe("test", () => {
-    it("should work", () => {
-        assert.equal(add(1,2), 3);
-    })
+describe('test', () => {
+  it('should work', () => {
+    assert.equal(add(1, 2), 3)
+  })
 })
--- wasm/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-wasm-pack build --target nodejs --out-dir pkg-node
-wasm-pack build --target web --out-dir pkg-web
